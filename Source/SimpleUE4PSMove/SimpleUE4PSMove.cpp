864b1c05e048227939bf82012e5f16e35a13d525
Chadwick Boulay <chadwick.boulay@gmail.com>
2015-08-17 23:18:44 -0400

    First commit.

---
+++ Source/SimpleUE4PSMove/SimpleUE4PSMove.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SimpleUE4PSMove.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimpleUE4PSMove, "SimpleUE4PSMove" );
---
+++ Source/SimpleUE4PSMove/SimpleUE4PSMoveGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SimpleUE4PSMoveGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLEUE4PSMOVE_API ASimpleUE4PSMoveGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
